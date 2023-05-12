37d372b3214ae5fa0e64758d8f5ebee56f252877
siddhanth kanukuntla <kanukuntlasiddhanth1@email.com>
2023-05-12 11:07:09 +0530

    added img

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ function App() {
   return (
     <div className="App">
       <h1>John Abraham</h1>
+      <h2>ggggg</h2>
+      <hhhhhhh2>gyag</hhhhhhh2>
+      <img src="https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg"></img>
+      <img src="https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-doi-inthanon-national-park-thailand-36703721.jpg"></img>
       <p>John Abraham (born 17 December 1972)[1] is an Indian actor and film producer who works in Hindi films. Known for his stoic action hero persona, he has won a National Film Award (as producer) and received nominations for five Filmfare Awards.After a successful modelling career, Abraham made his acting debut with the erotic thriller film Jism (2003), a sleeper hit.[2] He gained success with the action film Dhoom (2004) and expanded to comedic roles in Garam Masala (2005), Taxi No. 9211 (2006), and Dostana (2008). He also starred in the critically acclaimed dramas Water (2005), Kabul Express (2006) and New York (2009). In the subsequent decade, Abraham's career fluctuated commercially. He had successes in the ensemble films Housefull 2 (2012), Race 2 (2013), and Welcome Back (2015), and in the acclaimed political thriller Madras Cafe (2013). Abraham subsequently had his biggest commercial successes as a leading man in the action dramas Parmanu (2018), Satyameva Jayate (2018) and Batla House (2019). In 2023, Abraham starred in his highest-grossing release, the action film Pathaan.[3]</p>
     </div>
+
   );
 }
 
